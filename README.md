8dc1c0608ffe00a462ba9581c374b41425e51fcb
Kexin-Li <hollalikexin@gmail.com>
2019-01-22 13:55:53 +0800

    add spread.js

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # utils
+
 JavaScript object &amp; array util methods.
+
+## Object
+
+- 判断数组或者对象是否为空
+
+## Array
+
+- 创建二维数组
+
+## String
+
+- 替换字符串中的所有子串
+
+## Others
+
+- spread operation vs. Object.assign()
---
+++ spread.js
@@ -0,0 +1,37 @@
+// set属性
+Object.defineProperty(Object.prototype, "a", {
+  set(value) {
+    console.log("set called!");
+  }
+});
+
+const obj = { a: 10 };
+console.log("测试set属性：");
+console.log({ ...obj });
+console.log(Object.assign({}, obj));
+
+// 不可枚举属性
+const car = {
+  color: "blue"
+};
+
+Object.defineProperty(car, "type", {
+  value: "coupe",
+  enumerable: false
+});
+
+console.log("测试不可枚举属性：");
+console.log({ ...car });
+console.log(Object.assign({}, car));
+
+// 继承过来的属性
+const car2 = Object.create(car, {
+  type: {
+    value: "coupe",
+    enumerable: true
+  }
+});
+
+console.log("测试继承过来的属性：");
+console.log({ ...car2 });
+console.log(Object.assign({}, car2));

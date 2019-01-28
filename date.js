ebc9deeb3d51930667d938e9d039cd62bebbc1a6
Kexin-Li <hollalikexin@gmail.com>
2019-01-28 17:42:08 +0800

    feat: add dateJS

---
+++ date.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * 将utc时间转换为可显示的本地时间：2019-01-28T08:27:19+00:00 -> 2019-1-28 16:27:19
+ * @param {*} utc
+ */
+function utcToLocaleString(utc) {
+  return new Date(utc).toLocaleString();
+}

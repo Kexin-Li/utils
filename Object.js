2ffeda134ae489f421f7c2d42ac03200567714fb
Kexin-Li <hollalikexin@gmail.com>
2019-03-13 23:09:43 +0800

    add equal.js

--- Object.js
+++ Object.js
@@ -42,3 +42,26 @@ function parseQuery(input) {
 
   return ret;
 }
+
+/**
+ * 判断两个对象里的元素是否原生相等
+ * @param {Object} obj1
+ * @param {Object} obj2
+ */
+function equalObject(obj1, obj2) {
+  if (!obj1 && !obj2) return true;
+  if (!obj1) return false;
+  if (!obj2) return false;
+
+  for (let el in obj1) {
+    if (Object.prototype.hasOwnProperty.call(obj2, el)) {
+      // nested object
+      if (Object.prototype.toString.call(obj1[el]) === '[object Object]') {
+        if (!equalObject(obj1[el], obj2[el])) return false;
+      } else if (obj1[el] !== obj2[el]) return false;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
--- array.js
+++ array.js
@@ -51,3 +51,65 @@ function swap(arr, p, q) {
   [arr[p], arr[q]] = [arr[q], arr[p]];
   return arr;
 }
+
+/**
+ * 判断两个数组里的元素是否原生相等
+ * @param {Object} obj1
+ * @param {Object} obj2
+ */
+function equalArray(arr1, arr2) {
+  if (!arr1 || arr1.length < 0) return false;
+  if (!arr2 || arr2.length < 0) return false;
+  if (arr1.length !== arr2.length) return false;
+
+  const getIndex = (arr, target) => arr.findIndex(el => el === target);
+  return arr1.every(el1 => getIndex(arr1, el1) === getIndex(arr2, el1));
+}
+
+/**
+ * 判断两个数组或对象是否形态相等
+ * @param {Object | Array} val1
+ * @param {Object | Array} val2
+ */
+function equal(val1, val2) {
+  if (!val1 && !val2) return true;
+
+  const getType = value => Object.prototype.toString.call(value);
+  if (getType(val1) !== getType(val2)) return false;
+
+  const equalArray = (arr1, arr2) => {
+    if (!arr1 || arr1.length < 0) return false;
+    if (!arr2 || arr2.length < 0) return false;
+    if (arr1.length !== arr2.length) return false;
+
+    return arr1.every(target => arr2.findIndex(el => el == target) >= 0);
+  };
+
+  const equalObject = (obj1, obj2) => {
+    if (!obj1 && !obj2) return true;
+    if (!obj1) return false;
+    if (!obj2) return false;
+
+    for (let el in obj1) {
+      if (Object.prototype.hasOwnProperty.call(obj2, el)) {
+        // nested object
+        if (getType(obj1[el]) === '[object Object]') {
+          if (!equalObject(obj1[el], obj2[el])) return false;
+        } else if (getType(obj1[el]) === '[object Array]') {
+          if (!equalArray(obj1[el], obj2[el])) return false;
+        } else if (obj1[el] !== obj2[el]) return false;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  return getType(val1) === '[object Object]'
+    ? equalObject(val1, val2)
+    : equalArray(val1, val2);
+}
+
+const val1 = { a: [1, '2'], b: 2 };
+const val2 = { b: 2, a: [1, 2] };
+console.log(equal(val1, val2));

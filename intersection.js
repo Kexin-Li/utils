ef560683c44c61649a6e8cf8fe5f8439df4e8743
Kexin-Li <hollalikexin@gmail.com>
2019-10-16 13:42:57 +0800

    feat: add utils

---
+++ intersection.js
@@ -0,0 +1,20 @@
+/**
+ * 获取两个数组的交集(不支持嵌套数组)
+ *
+ * @param {array} a
+ * @param {array} b
+ * @returns {array}
+ * @example
+ *
+ * intersection([1,2,3], [1,3,4])
+ * // => [1,3]
+ *
+ * intersection([1,2,3], [4,5,6])
+ * // => []
+ */
+const intersection = (a, b) => {
+  const s = new Set(b);
+  return a.filter(x => s.has(x));
+};
+
+export default intersection;
---
+++ intersectionBy.js
@@ -0,0 +1,6 @@
+const intersectionBy = (a, b, fn) => {
+  const s = new Set(b.map(fn));
+  return a.filter(x => s.has(fn(x)));
+};
+
+export default intersectionBy;
